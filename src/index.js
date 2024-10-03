dc660eea8c23e7df1961c6983623db8995da5e3a
ANAS-ALGO-TECH <anasalgotech9@gmail.com>
2024-10-03 18:45:28 +0500

    added the most required middlewares in the app.js, and now working on the utils i.e. custom error, api response and reusability of the code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import mongoose from "mongoose";
-import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({ path: "./env" });
 
-const app = express();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("ERROR: ", error);
+      throw error;
+    });
 
-connectDB();
+    app.listen(`${process.env.PORT}`, () => {
+      console.log(`App is listening on Port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection Failed !!! ", error);
+  });
 
 /*
 (async () => {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+//! Method - 1 (using async/await and try/catch)
+// export const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+
+//! Method - 2 (using promises)
+export const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export default asyncHandler;
